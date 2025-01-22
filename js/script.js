18dbcbbf22d286ba48964aa0b8bb32b38c89c383
Mirko <mirkocaccavone03@gmail.com>
2025-01-22 15:02:41 +0100

    [feat] add click event on the button

--- js/script.js
+++ js/script.js
@@ -7,27 +7,35 @@
 // Abbellire con CSS o Bootstrap
 // Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 
+
+
+// settiamo le variabili
 const list = document.getElementById('container');
+const button = document.getElementById('bottone')
 
-// Creare una lista di 10 indirizzi email
-// Creare un ciclo for che generi 10 indirizzi email
 
-for (let i = 0; i < 10; i++){
+// Creare un evento click sul bottone
+button.addEventListener('click', () => {
 
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(response => {
-        // console.log(response);
-        
-        const email = response.data.response;
-        console.log(email);
+    // Svuotare la lista ad ogni click
+    list.innerHTML = '';
 
-        list.innerHTML += `<li>${email}</li>`;
+    // Creare un ciclo for che generi 10 indirizzi email
+    for (let i = 0; i < 10; i++){
 
-        
-    });
-};
+        // Creare una chiamata API per generare un indirizzo email
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            // console.log(response);
+            
+            const email = response.data.response;
+            console.log(email);
 
-// Creare una chiamata API per generare un indirizzo email
+            // Stampare in pagina gli indirizzi email
+            list.innerHTML += `<li>${email}</li>`; 
+        });
+    };
+});
 
-// Stampare in pagina gli indirizzi email generati
+
 
