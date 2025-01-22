6602753a2a9b78b52f0bf49d36248e91787717a7
Mirko <mirkocaccavone03@gmail.com>
2025-01-22 15:32:52 +0100

    [feat] add array to save lists

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-// Descrizione
-// Attraverso l’apposita API di Boolean
-//  https://flynn.boolean.careers/exercises/api/random/mail
-// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 
-// Bonus
-// Abbellire con CSS o Bootstrap
-// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
-
-
-
-// settiamo le variabili
+// settiamo le variabili 
 const list = document.getElementById('container');
 const button = document.getElementById('bottone')
 
@@ -17,9 +7,12 @@ const button = document.getElementById('bottone')
 // Creare un evento click sul bottone
 button.addEventListener('click', () => {
 
+    // Array vuoto per salvare le liste
+    let listaEmail =[];
+
     // Svuotare la lista ad ogni click
     list.innerHTML = '';
-
+    
     // Creare un ciclo for che generi 10 indirizzi email
     for (let i = 0; i < 10; i++){
 
@@ -27,15 +20,20 @@ button.addEventListener('click', () => {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(response => {
             // console.log(response);
-            
+           
             const email = response.data.response;
-            console.log(email);
+            // console.log(email);
+            listaEmail.push(email);
+           
 
             // Stampare in pagina gli indirizzi email
             list.innerHTML += `<li>${email}</li>`; 
         });
+        
     };
+    // Console log per vedere tutte le liste di email
+    console.log(listaEmail);
 });
 
-
+ 
 
